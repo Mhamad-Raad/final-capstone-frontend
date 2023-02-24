828b15f7f300a0b0a3d8d01cdcda7aacb6ef51f6
joker69216 <hamaraad883@gmail.com>
2023-02-24 17:49:12 +0300

    install redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+    );
