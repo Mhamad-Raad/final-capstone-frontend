fa8c179b35bb87084638b4a74a60d997d78921d0
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-27 01:29:05 +0500

    Handle the navigation and routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
-import Signup from './components/Signup';
+import {
+  Route, useNavigate, Routes, useLocation,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Signup from './pages/Signup';
+import SignIn from './pages/SignIn';
 import Trips from './components/Trips';
+import { setToken } from './redux/reducers/registrationSlice';
 
-const App = () => (
-  <Router>
+const App = () => {
+  const registration = useSelector((store) => store.registration);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const token = (localStorage.getItem('token') !== null);
+    const user = (localStorage.getItem('user') !== null);
+
+    if (token && user) {
+      setToken({
+        token,
+        user,
+      });
+    } else if (location.pathname !== '/sign-up') {
+      navigate('/sign-in');
+    }
+  }, [registration]);
+
+  return (
     <Routes>
       <Route exact path="/sign-up" element={<Signup />} />
+      <Route exact path="/sign-in" element={<SignIn />} />
       <Route exact path="/trips" element={<Trips />} />
     </Routes>
-  </Router>
-);
+  );
+};
 
 export default App;
