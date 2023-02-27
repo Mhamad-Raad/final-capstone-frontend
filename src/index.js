fb4def3aff8b149cb94f09a9efda7d46ff47371e
joker69216 <hamaraad883@gmail.com>
2023-02-27 17:54:48 +0300

    fix eslint errors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import store from './redux/store';
 import './index.css';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
