7b6b2ac525be7518457fd3e51ab2420404f170b7
joker69216 <hamaraad883@gmail.com>
2023-02-27 17:49:03 +0300

    reset index.js file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
-// import App from './App';
-import Details from './components/DetailsPage/Details';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Details />
+    <App />
   </Provider>,
 );
