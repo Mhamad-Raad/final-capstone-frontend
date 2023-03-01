3b21797890ab29398d9522828e39dd23507c4c2e
shakerAbuDrais <thecoffeiner@gmail.com>
2023-03-01 20:15:26 +0300

    Fix linters

--- src/components/AddTripForm.jsx
+++ src/components/AddTripForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addTrip } from '../redux/tripSlice';
 
 const AddTripForm = () => {
@@ -11,14 +11,14 @@ const AddTripForm = () => {
   const user = JSON.parse(localStorage.getItem('user')) || {};
   const userId = user.id;
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const tripData = {
       price,
       rating,
       destination_city: destinationCity,
       description,
-      user_id: userId
+      user_id: userId,
     };
     dispatch(addTrip(tripData));
     setPrice('');
@@ -30,38 +30,46 @@ const AddTripForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="price">Price:</label>
+        <label htmlFor="price">
+            Price:
+        </label>
         <input
           type="number"
           id="price"
           value={price}
-          onChange={e => setPrice(e.target.value)}
+          onChange={(e) => setPrice(e.target.value)}
         />
       </div>
       <div>
-        <label htmlFor="rating">Rating:</label>
+        <label htmlFor="rating">
+            Rating:
+        </label>
         <input
           type="number"
           id="rating"
           value={rating}
-          onChange={e => setRating(e.target.value)}
+          onChange={(e) => setRating(e.target.value)}
         />
       </div>
       <div>
-        <label htmlFor="destinationCity">Destination City:</label>
+        <label htmlFor="destinationCity">
+            Destination City:
+        </label>
         <input
           type="text"
           id="destinationCity"
           value={destinationCity}
-          onChange={e => setDestinationCity(e.target.value)}
+          onChange={(e) => setDestinationCity(e.target.value)}
         />
       </div>
       <div>
-        <label htmlFor="description">Description:</label>
+        <label htmlFor="description">
+            Description:
+        </label>
         <textarea
           id="description"
           value={description}
-          onChange={e => setDescription(e.target.value)}
+          onChange={(e) => setDescription(e.target.value)}
         />
       </div>
       <button type="submit">Add Trip</button>
