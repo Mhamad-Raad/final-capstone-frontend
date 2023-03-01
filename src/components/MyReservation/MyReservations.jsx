fc89766201830d0560a42c771ab370d523a8c977
joker69216 <hamaraad883@gmail.com>
2023-03-02 00:49:36 +0300

    add show database reservations

--- src/components/MyReservation/MyReservations.jsx
+++ src/components/MyReservation/MyReservations.jsx
@@ -1,9 +1,10 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllReversedTrips } from '../../redux/reserveSlice';
 import ReservationItem from './ReservationItem';
 import './MyReservations.css';
 
 export default function MyReservations() {
+  const reservations = useSelector((store) => store.reserveItems.reserved);
   const dispatch = useDispatch();
 
   const clickHandler = () => {
@@ -12,6 +13,9 @@ export default function MyReservations() {
 
   return (
     <ul className="my-reservations column">
+      {reservations.map((reservation, index) => (
+        <ReservationItem key={reservation.id} reservation={reservation} delay={`1.${index}`} />
+      ))}
       <ReservationItem delay="1" />
       <ReservationItem delay="1.2" />
       <ReservationItem delay="1.3" />
