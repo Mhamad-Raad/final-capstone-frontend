5cf2b1f05516ae728222dd0916e00c185cd75fdf
shakerAbuDrais <thecoffeiner@gmail.com>
2023-03-01 20:30:12 +0300

    Fix linters

--- src/components/AddTripForm.jsx
+++ src/components/AddTripForm.jsx
@@ -3,78 +3,78 @@ import { useDispatch } from 'react-redux';
 import { addTrip } from '../redux/tripSlice';
 
 const AddTripForm = () => {
-  const [price, setPrice] = useState('');
-  const [rating, setRating] = useState('');
-  const [destinationCity, setDestinationCity] = useState('');
-  const [description, setDescription] = useState('');
-  const dispatch = useDispatch();
-  const user = JSON.parse(localStorage.getItem('user')) || {};
-  const userId = user.id;
+    const [price, setPrice] = useState('');
+    const [rating, setRating] = useState('');
+    const [destinationCity, setDestinationCity] = useState('');
+    const [description, setDescription] = useState('');
+    const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('user')) || {};
+    const userId = user.id;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const tripData = {
-      price,
-      rating,
-      destination_city: destinationCity,
-      description,
-      user_id: userId,
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const tripData = {
+            price,
+            rating,
+            destination_city: destinationCity,
+            description,
+            user_id: userId,
+        };
+        dispatch(addTrip(tripData));
+        setPrice('');
+        setRating('');
+        setDestinationCity('');
+        setDescription('');
     };
-    dispatch(addTrip(tripData));
-    setPrice('');
-    setRating('');
-    setDestinationCity('');
-    setDescription('');
-  };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="price">
-          Price:
-        <input
-          type="number"
-          id="price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="rating">
-          Rating:
-        </label>
-        <input
-          type="number"
-          id="rating"
-          value={rating}
-          onChange={(e) => setRating(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="destinationCity">
-          Destination City:
-        </label>
-        <input
-          type="text"
-          id="destinationCity"
-          value={destinationCity}
-          onChange={(e) => setDestinationCity(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="description">
-          Description:
-        </label>
-        <textarea
-          id="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </div>
-      <button type="submit">Add Trip</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="price">
+                    Price:
+                    <input
+                        type="number"
+                        id="price"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label htmlFor="rating">
+                    Rating:
+                </label>
+                <input
+                    type="number"
+                    id="rating"
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="destinationCity">
+                    Destination City:
+                </label>
+                <input
+                    type="text"
+                    id="destinationCity"
+                    value={destinationCity}
+                    onChange={(e) => setDestinationCity(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="description">
+                    Description:
+                </label>
+                <textarea
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            </div>
+            <button type="submit">Add Trip</button>
+        </form>
+    );
 };
 
 export default AddTripForm;
