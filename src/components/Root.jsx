9fd41cd871e369de53d72f285e538fd23bec5bf2
joker69216 <hamaraad883@gmail.com>
2023-03-08 03:06:16 +0300

    add close and open button for mobile navbar

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,10 +1,22 @@
 import { Outlet, NavLink, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import '../assets/stylesheets/root.css';
 
 const RootLayout = () => {
   const location = useLocation();
   const { pathname } = location;
   const isReserve = pathname.includes('reserve');
+
+  const [opened, setOpened] = useState(false);
+
+  const closeHandler = () => {
+    setOpened(false);
+  };
+
+  const openHandler = () => {
+    setOpened(true);
+  };
+
   return (
     <div className="root-layout">
       <nav className="desktop-nav column">
@@ -38,6 +50,63 @@ const RootLayout = () => {
             <a href="https://www.linkedin.com/in/kanza-tahreem/">Kanza Tahreem</a>
           </div>
         </div>
+      </nav>
+      <nav className="mobile-nav row">
+        {!opened && (
+        <button
+          className="hamburger"
+          onClick={openHandler}
+          type="button"
+        >
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+        </button>
+        )}
+
+        {opened && (
+          <button
+            className="close"
+            onClick={closeHandler}
+            type="button"
+          >
+            <div className="line1" />
+            <div className="line2" />
+          </button>
+        )}
+        {opened && <div className="overlay" />}
+        {/* <div className="mob-nav-logo">
+          <h1 className="mob-nav-logo">Travel App</h1>
+          <ul className="row">
+            <li>
+              <NavLink to="home">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/add-trip">Add New Trip</NavLink>
+            </li>
+            <li>
+              <NavLink to="/my-reservations">My Reservations</NavLink>
+            </li>
+            <li>
+              <NavLink to="/delete-trip">Delete a Trip</NavLink>
+            </li>
+            <li>
+              <NavLink to="/reserve/0" className={isReserve ? 'active' : ''}>
+                Reserve a Trip
+              </NavLink>
+            </li>
+          </ul>
+          <div className="mob-copy-right row">
+            <p>
+              &copy; 2021 Travel App made by:
+            </p>
+            <div className="creator-names row">
+              <a href="https://www.linkedin.com/in/mhamad-raad">Mhamad Raad</a>
+              <a href="https://www.linkedin.com/in/shaker-abady">Shaker Abady</a>
+              <a href="https://www.linkedin.com/in/kanza-tahreem/">Kanza Tahreem</a>
+            </div>
+          </div>
+        </div> */}
       </nav>
       <Outlet />
     </div>
