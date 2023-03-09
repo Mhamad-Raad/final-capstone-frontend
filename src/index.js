61ad0efbc11ce3cad1c8980db45d7a2fbe448103
KanzaTahreem <tehreemmsk411@gmail.com>
2023-03-10 02:16:31 +0500

    Update index.css file location

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
-import './index.css';
+import './assets/stylesheets/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
